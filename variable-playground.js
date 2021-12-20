cbf370b47529f8b24201e956edd2b53267dd7aa3
Cam Thu <nguyenthicamthu1112@gmail.com>
2021-12-20 22:35:25 +0700

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+/*
+
+var person = {
+    name: 'Andrew',
+    age: 21
+};
+
+function updatePerson (obj){
+
+    obj.age = 24;
+}
+
+updatePerson(person);
+console.log(person);
+
+//  Array example
+
+*/
+
+var a =  [17, 35];
+
+function updateArray (ab) {
+    ab.push(20);
+    debugger;
+   
+}
+
+updateArray(a);
+console.log(a);
