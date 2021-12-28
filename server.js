f006cbead8176222645ee5d49b168e50b8ace190
Cam Thu <nguyenthicamthu1112@gmail.com>
2021-12-28 22:33:54 +0700

    Add todo model and POST/todos

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require ('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
+
+
 const { filter } = require('underscore');
 
 var app = express ();
@@ -64,7 +67,13 @@ app.post('/todos', function(req, res){
 
 	var body = _.pick(req.body,'description', 'completed');
 
+	db.todo.create(body).then(function(todo){
+		res.json(todo.toJSON());
+	}, function(e){
+		res.status(400).json(e);
+	});
 
+	/*
 	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ){
 		return res.status(400).send();
 	}
@@ -80,7 +89,7 @@ app.post('/todos', function(req, res){
 	//console.log ('description: '+  body.description);
 
 	res.json(body);
-
+	*/
 });
 
 
@@ -153,7 +162,12 @@ app.put('/todos/:id', function (req, res){
 });
 
 
-app.listen (PORT, function (){
-	console.log ('Express listening on port ' + PORT + '!' ) ;
+db.sequelize.sync().then(function(){
+	app.listen (PORT, function (){
+		console.log ('Express listening on port ' + PORT + '!' ) ;
+	
+	} );  
+});
 
-} );  
+
+
